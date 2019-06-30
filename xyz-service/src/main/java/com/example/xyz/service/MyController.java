fa5b3c31f71d1c593e0df0d010b514fef46c810c
Sofia <sofiaguyang@users.noreply.github.com>
2019-06-30 22:53:32 +0800

    Initialize poc

---
+++ xyz-service/src/main/java/com/example/xyz/service/MyController.java
@@ -0,0 +1,40 @@
+package com.example.xyz.service;
+
+import org.jeasy.rules.api.Facts;
+import org.jeasy.rules.api.Rules;
+import org.jeasy.rules.api.RulesEngine;
+import org.jeasy.rules.core.DefaultRulesEngine;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @Autowired
+    @Qualifier("firstRules")
+    Rules firstRules;
+
+    @Autowired
+    @Qualifier("secondRules")
+    Rules secondRules;
+
+    @GetMapping
+    String firstRulesEngineEndpoint() {
+        RulesEngine rulesEngine = new DefaultRulesEngine();
+        Facts facts = new Facts();
+        facts.put("hello", "world");
+        rulesEngine.fire(firstRules, facts);
+        return facts.get("hello");
+    }
+
+    @GetMapping
+    String secondRulesEngineEndpoint() {
+        RulesEngine rulesEngine = new DefaultRulesEngine();
+        Facts facts = new Facts();
+        facts.put("hello", "world");
+        rulesEngine.fire(secondRules, facts);
+        return facts.get("hello");
+    }
+}
